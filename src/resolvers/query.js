d81cab416f4d353a8c6ccbe5e72ac3f99ab017c4
autsada <autsada.t@gmail.com>
2020-03-02 15:17:59 +0700

    Sorted products in user

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,37 +1,38 @@
-import User from "../models/user"
-import Product from "../models/product"
+import User from '../models/user'
+import Product from '../models/product'
 
 const Query = {
   user: (parent, args, { userId }, info) => {
     // Check if user logged in
-    if (!userId) throw new Error("Please log in")
+    if (!userId) throw new Error('Please log in')
 
     return User.findById(userId)
       .populate({
-        path: "products",
-        populate: { path: "user" }
+        path: 'products',
+        options: { sort: { createdAt: 'desc' } },
+        populate: { path: 'user' }
       })
-      .populate({ path: "carts", populate: { path: "product" } })
+      .populate({ path: 'carts', populate: { path: 'product' } })
   },
   users: (parent, args, context, info) =>
     User.find({})
       .populate({
-        path: "products",
-        populate: { path: "user" }
+        path: 'products',
+        populate: { path: 'user' }
       })
-      .populate({ path: "carts", populate: { path: "product" } }),
+      .populate({ path: 'carts', populate: { path: 'product' } }),
   product: (parent, args, context, info) =>
     Product.findById(args.id).populate({
-      path: "user",
-      populate: { path: "products" }
+      path: 'user',
+      populate: { path: 'products' }
     }),
   products: (parent, args, context, info) =>
     Product.find()
       .populate({
-        path: "user",
-        populate: { path: "products" }
+        path: 'user',
+        populate: { path: 'products' }
       })
-      .sort({ createdAt: "desc" })
+      .sort({ createdAt: 'desc' })
 }
 
 export default Query
